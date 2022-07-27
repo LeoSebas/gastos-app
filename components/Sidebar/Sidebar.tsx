f2789adb6dcccf87d31f4ac7108fb2e6f426cfb6
R4vage <julianmarc@gmail.com>
2022-07-27 20:18:05 -0300

    Fixes, mejorado search y otras chauchas

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -7,15 +7,17 @@ import styles from  "./Sidebar.module.css"
 export default function Sidebar() {
     const [sideOpen, setSideOpen] = useState(false)
 
-    return (
-            <nav className={sideOpen?`${styles.Sidebar} ${styles.Sidebar__open}`:`${styles.Sidebar}`}>
-                <div className={styles.Sidebar__hamburger__container} onClick={()=>setSideOpen(!sideOpen)}><Hamburger isOpen={sideOpen}/></div>
-                <div className={styles.Sidebar__list}>
-                    <Button text="Home" link="home"/>
-                    <Button text="Gastos" link="search"/>
-                    <Button text="Categorias" link=""/>
-                    <Button text="Perfil" link=""/>
-                </div>
-            </nav>
+    return (<>
+                <div className={sideOpen?styles.pusherOpen:styles.pusher}></div> 
+                <nav className={sideOpen?`${styles.Sidebar} ${styles.Sidebar__open}`:`${styles.Sidebar}`}>
+                    <div className={styles.Sidebar__hamburger__container} onClick={()=>setSideOpen(!sideOpen)}><Hamburger isOpen={sideOpen}/></div>
+                    <div className={styles.Sidebar__list}>
+                        <Button text="Home" link="home"/>
+                        <Button text="Gastos" link="search"/>
+                        <Button text="Categorias" link=""/>
+                        <Button text="Perfil" link=""/>
+                    </div>
+                </nav>
+            </>
     )
 }

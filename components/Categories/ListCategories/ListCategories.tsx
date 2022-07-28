221101246ed96d3e72aad3a7e0e1a6efea60521a
R4vage <julianmarc@gmail.com>
2022-07-28 16:33:56 -0300

    Listado de Categorias

--- components/Categories/ListCategories/ListCategories.tsx
+++ components/Categories/ListCategories/ListCategories.tsx
@@ -6,9 +6,9 @@ export default function ListCategories (props){
     const{ userCategories } = props
     console.log(userCategories)
     return (
-        <div>
+        <div className={style.ListCategories}>
             {userCategories?.map((category)=> <CategoryItem key={category._id} categoryID={category._id} name={category.name} color={category.color} options={true}/>)}
-
+            
         </div>
     )
 }

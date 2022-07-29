dae23cf32114080864bd21a8cdee94a2d79de496
R4vage <julianmarc@gmail.com>
2022-07-29 14:29:12 -0300

    Fixes positions

--- components/Sidebar/Button.tsx
+++ components/Sidebar/Button.tsx
@@ -4,5 +4,5 @@ import Link from "next/link";
 export default function Button(props) {
     const {text, link} = props
 
-    return <Link href ={`/private/${link}`} ><span><button ><a>{text}</a></button></span></Link>
+    return <Link href ={`/private/${link}`} ><button ><a>{text}</a></button></Link>
 }

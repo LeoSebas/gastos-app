bf4d8c9eedef20d4bddc49e7c660567274c87937
Leonardo Canteros <leo.sebas.065@gmail.com>
2022-07-31 15:26:54 -0300

    Fixed useEffect

--- pages/private/home/index.tsx
+++ pages/private/home/index.tsx
@@ -34,7 +34,7 @@ export default function Home() {
         }
 
         categories()
-    })
+    }, [user, dispatch])
 
     useEffect(() => {
         const getRecentAddedExpensesList = async () => {
